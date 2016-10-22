66cde0461a6e4c107922b540354e702a987459c4
frankmalo86 <frankmalo86@gmail.com>
2016-10-22 15:23:52 -0500

    Intents

--- app/src/main/java/curso/android/cisco/holamundo/MainActivity.java
+++ app/src/main/java/curso/android/cisco/holamundo/MainActivity.java
@@ -1,9 +1,11 @@
 package curso.android.cisco.holamundo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -30,11 +32,51 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "Otro mensaje", Toast.LENGTH_SHORT).show();
             }
         });*/
+        Toast.makeText(this, "Método On Create", Toast.LENGTH_SHORT).show();
+
     }
 
     public void presionar_boton(View view){
         final String mensaje = getResources().getString(R.string.alerta);
         Toast.makeText(MainActivity.this, mensaje, Toast.LENGTH_SHORT).show();
+
+        EditText tvUsuario = (EditText) findViewById(R.id.tvUsuario);
+        String usuario = tvUsuario.getText().toString();
+        Intent intent = new Intent(this, TableLayout.class);
+        String key1 = getResources().getString(R.string.parametro1);
+        intent.putExtra(key1, usuario);
+
+        startActivity(intent);
+
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "Método On Start", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "Método On Resume", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "Método On Pause", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "Método On Stop", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "Método On Destroy", Toast.LENGTH_SHORT).show();
+    }
 }
--- app/src/main/java/curso/android/cisco/holamundo/TableLayout.java
+++ app/src/main/java/curso/android/cisco/holamundo/TableLayout.java
@@ -2,6 +2,7 @@ package curso.android.cisco.holamundo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class TableLayout extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class TableLayout extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_table_layout);
+
+
+        String key1 = getResources().getString(R.string.parametro1);
+        Bundle bundle = getIntent().getExtras();
+        String usuario = bundle.getString(key1);
+
+        TextView tvUsuario = (TextView)findViewById(R.id.tvUsuario);
+        tvUsuario.setText("Bienvenido: " + usuario);
+
     }
 }
