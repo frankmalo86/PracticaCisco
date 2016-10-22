9676259eb0762f21e1e2d6f87611706ddd26d30f
frankmalo86 <frankmalo86@gmail.com>
2016-10-22 15:43:33 -0500

    Intent implicito

--- app/src/main/java/curso/android/cisco/holamundo/actividad1.java
+++ app/src/main/java/curso/android/cisco/holamundo/actividad1.java
@@ -1,7 +1,10 @@
 package curso.android.cisco.holamundo;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class actividad1 extends AppCompatActivity {
 
@@ -10,4 +13,17 @@ public class actividad1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad1);
     }
+
+
+    public void enviarMail(View v){
+        String email = "frankmalo86@gmail.com";
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, email);
+        //puedo tener más opciones // cc, subject, etc.
+        emailIntent.setType("message/rfc822");
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Mi primer mail desde Android");
+        startActivity(Intent.createChooser(emailIntent, "Email"));
+    }
+
 }
